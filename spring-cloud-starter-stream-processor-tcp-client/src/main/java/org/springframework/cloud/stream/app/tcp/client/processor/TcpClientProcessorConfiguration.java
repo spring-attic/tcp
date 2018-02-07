1de182d9469e7adda4ca15901918169ae60b46bb
Christian Tzolov <christian.tzolov@gmail.com>
2018-02-07 11:21:51 +0100

    Resolves #4, #5, #6, #7 and #8

--- spring-cloud-starter-stream-processor-tcp-client/src/main/java/org/springframework/cloud/stream/app/tcp/client/processor/TcpClientProcessorConfiguration.java
+++ spring-cloud-starter-stream-processor-tcp-client/src/main/java/org/springframework/cloud/stream/app/tcp/client/processor/TcpClientProcessorConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 the original author or authors.
+ * Copyright 2016-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,6 +37,8 @@ import org.springframework.integration.ip.tcp.serializer.AbstractByteArraySerial
  *
  * @author Ilayaperumal Gopinathan
  * @author Gary Russell
+ * @author Christian Tzolov
+ *
  */
 @EnableBinding(Processor.class)
 @EnableConfigurationProperties({TcpClientProcessorProperties.class, TcpConnectionFactoryProperties.class})
--- spring-cloud-starter-stream-source-tcp-client/src/main/java/org/springframework/cloud/stream/app/tcp/client/source/TcpClientSourceConfiguration.java
+++ spring-cloud-starter-stream-source-tcp-client/src/main/java/org/springframework/cloud/stream/app/tcp/client/source/TcpClientSourceConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2017 the original author or authors.
+ * Copyright 2016-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,6 +35,7 @@ import org.springframework.integration.ip.tcp.serializer.AbstractByteArraySerial
  *
  * @author Ilayaperumal Gopinathan
  * @author Gary Russell
+ * @author Christian Tzolov
  */
 @EnableBinding(Source.class)
 @EnableConfigurationProperties({ TcpClientSourceProperties.class, TcpConnectionFactoryProperties.class })
